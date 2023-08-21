b27515bac756059813e3759d7b15d9871802afd6
Alejandro La Loggia <54778764+AlejandroLaLoggia@users.noreply.github.com>
2023-08-21 12:20:38 -0300

    Primer commit, subidos html e imagenes

---
+++ js/app.js
@@ -0,0 +1,8 @@
+document.querySelector(`#header`).innerHTML=`
+<ul class="header__ul">
+            <li class="header__li"><a class="header__a" title="Boton de Inicio" href="index.html">INICIO</a></li>
+            <li class="header__li"><a class="header__a" title="Boton de Galeria" href="galeria.html">GALERIA</a></li>
+            <li class="header__li"><a class="header__a" title="Boton de Info" href="info.html">INFO</a></li>
+            <li class="header__li"><a class="header__a" title="Boton de Registro" href="registro.html">REGISTRO</a></li>
+</ul>
+`;
